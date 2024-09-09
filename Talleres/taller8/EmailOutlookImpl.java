4bd475ee255506b29de86ba61999f0e49d0efe8a
ManuelGallego1 <107779249+ManuelGallego1@users.noreply.github.com>
2024-09-09 17:31:08 -0500

    Add files via upload

--- Talleres/taller8/EmailOutlookImpl.java
+++ Talleres/taller8/EmailOutlookImpl.java
@@ -1,10 +1,14 @@
 package taller8;
 
-public class EmailOutlookImpl implements Iemail{
+public class EmailOutlookImpl implements IServicioEmail{
 
     @Override
     public void enviarEmail(Email email) {
         EmailOutlook outlook = (EmailOutlook) email;
+        System.out.println(outlook.getTo());
+        System.out.println(outlook.getSubject());
+        System.out.println(outlook.getBody());
+        
         System.err.println("ENVIANDO CORREO POR OUTLOOK");
     }
     
